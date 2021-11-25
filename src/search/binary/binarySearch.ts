e193a3987d7202879cff40a7c552e50e6f9f1f13
Year 12 <year12@mar-admins-MacBook-Air-4.local>
2021-11-25 09:41:49 +0000

    commit

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,6 +1,15 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
+    while(item != item){
+        let start = 0;
+        let end = items.length;
+        let mid=Math.floor((start + end)/2);
+        if (mid == item)
+        return mid
+
+    }
+
 
     return NO_MATCH;
 }
