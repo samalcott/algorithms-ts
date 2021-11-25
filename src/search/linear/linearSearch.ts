2a1f3af7ab103e6abad7d9f47da5bbd45bed8351
Year 12 <year12@mar-admins-MacBook-Air-4.local>
2021-11-25 09:12:34 +0000

    save

--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -1,7 +1,10 @@
 import { NO_MATCH } from "../common";
 
 const linearSearch = (items: any[], item: any): number => {
-
+    for(let i=0; i <items.length; i++){
+        if (items[i] == item)
+        return i;
+    }
     return NO_MATCH;
 }
 
