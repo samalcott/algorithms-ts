61bf853ac4172dbeb4d777226bebed3c93446090
Year 12 <year12@mar-admins-MacBook-Air-4.local>
2021-12-02 09:48:49 +0000

    bubble sort

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,14 +1,26 @@
 import { NO_MATCH } from "../common"
 
-const binarySearch = (items: any[], item: any): number => {
-    while(item != item){
-        let start = 0;
-        let end = items.length;
-        let mid=Math.floor((start + end)/2);
-        if (mid == item)
-        return mid
 
-    }
+
+
+const binarySearch = (items: any[], item: any): number => {
+ let start = 0;
+ let end = items.length - 1;
+ let found = false;
+ 
+while(found = false && start > end){
+    let mid=Math.floor((start + end)/2);
+ if (items[mid] == item){
+    let found = true;
+}else if (items[mid] > item){
+    start = mid + 1;
+}else if (items[mid] < item){
+    end = mid -1;
+}
+return mid
+
+}
+
 
 
     return NO_MATCH;
--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -2,7 +2,24 @@ import { SortAlgorithm } from "../common";
 import swap from "../swap";
 
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
+    let current = 0;
+    let end = items.length;
+    let swap = false;
+    let ordered_list = false;
+
+for(current=0; current< end; current++){
     
+    if(current == end){
+        end = items.length - 1;
+    }
+
+}
+  
+
+
+
+    
+
 
     // Your code here
 
